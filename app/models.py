d019acd2eb2fb4cf308801f277e5d8723515f93a
Vanpaia <h.schlotzhauer@gmail.com>
2024-06-10 17:59:10 +0200

    adding category table and auxiliary relationship table between article and category

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from app import db
 from datetime import datetime
 
 
+article_category = db.Table('category',
+    db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     capture_timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -12,3 +18,19 @@ class Article(db.Model):
     image = db.Column(db.String(128))
     publish_timestamp = db.Column(db.DateTime, index=True)
     summary = db.Column(db.Text)
+
+    categories = db.relationship(
+        'NewsCategory', secondary=article_category,
+        primaryjoin=(article_category.c.category_id == id),
+        backref=db.backref('categories', lazy='dynamic'), lazy='dynamic')
+
+class NewsCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    supercategory = db.Column(db.String(56), index=True)
+    link = db.Column(db.String(128), index=True)
+
+    articles = db.relationship(
+        'Article', secondary=article_category,
+        primaryjoin=(article_category.c.article_id == id),
+        backref=db.backref('articles', lazy='dynamic'), lazy='dynamic')
