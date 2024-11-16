c6e4e7e85559180620a8f54b14712505491252dd
Vanpaia <h.schlotzhauer@gmail.com>
2024-11-16 13:34:18 +0100

    fixed updated model names in nos_archief.py

--- nos_archief.py
+++ nos_archief.py
@@ -1,9 +1,9 @@
 from app import create_app, db
-from app.models import Article, NewsCategory
+from app.models import RSSArticle, RSSCategory
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Article': Article, 'NewsCategory': NewsCategory}
+    return {'db': db, 'RSSArticle': RSSArticle, 'RSSCategory': RSSCategory}
